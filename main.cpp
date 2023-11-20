54a7e4ce313cce69772b15ff8b0c190abf73dd0d
Alexisbigeaud <alexis.bigeaud@etu.estia.fr>
2023-11-20 16:39:49 +0100

    change frequency

--- main.cpp
+++ main.cpp
@@ -3,15 +3,31 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "mbed.h"
-
+InterruptIn button(BUTTON1);
+DigitalOut led(LED1);
+Ticker ticker;
 namespace {
 #define PERIOD_MS 2000ms
+}
+void toggleLED() {
+    led = !led;
+}
+
+void changeFrequency() {
+    static int frequency = 1;
+    ticker.detach();
+    ticker.attach(toggleLED, PERIOD_MS / frequency);
+    frequency *= 2;
+    if (frequency > 16) {
+        frequency = 1;
+    }
 }
 
 int main()
 {
-	while (true) {
-		printf("Alive! \n");
-		ThisThread::sleep_for(PERIOD_MS / 2);
-	}
+    button.rise(&changeFrequency);
+    ticker.attach(toggleLED, PERIOD_MS);
+    while (true) {
+        ThisThread::sleep_for(100ms);
+    }
 }
