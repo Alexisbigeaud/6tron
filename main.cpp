d5c26283b1c42ead32e6ff5a8e40ea497988820b
Alexisbigeaud <alexis.bigeaud@etu.estia.fr>
2023-11-21 15:15:19 +0100

    ping pong mutex

--- main.cpp
+++ main.cpp
@@ -3,38 +3,43 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
-
 #include "mbed.h"
 DigitalOut led1(LED1);
-namespace{
-	#define PERIOD_MS 1000ms
+
+Mutex printf_mutex;
+
+void ping() {
+    for(int i = 0; i < 100; i++) {
+        printf_mutex.lock();
+        printf("ping\n");
+        printf_mutex.unlock();
+
+    }
 }
 
-void ping(){
+void pong() {
+    for(int i = 0; i < 100; i++) {
+        printf_mutex.lock();
+        printf("pong\n");
+        printf_mutex.unlock();
 
-	for(int i = 0; i < 100; i++){
-		printf("ping\n");
-	}
-}
-
-void pong(){
-	for(int i = 0; i < 100; i++){
-		printf("pong\n");
-	}
+    }
 }
 
 int main() {
+    Thread ping_thread;
+    Thread pong_thread;
 
-	Thread ping_thread;
-	Thread pong_thread;
-	ping_thread.start(ping);
-	pong_thread.start(pong);
-	while(1){
-		led1 = !led1;
-		printf("Alive\n");
-		ThisThread::sleep_for(PERIOD_MS);
-	}
+    ping_thread.start(ping);
+    pong_thread.start(pong);
+	ping_thread.set_priority(osPriorityRealtime1);
+	pong_thread.set_priority(osPriorityRealtime2);
 
-
+    while(1) {
+        led1 = !led1;
+        printf_mutex.lock();
+        printf("Alive\n");
+        printf_mutex.unlock();
+        ThisThread::sleep_for(1000ms);
+    }
 }
